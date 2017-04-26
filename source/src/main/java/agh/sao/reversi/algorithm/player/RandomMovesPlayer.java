aabd3a9d66f1e34463d7e60842722c700fb6546b
Imiolak <maciej.imiolek@gmail.com>
2017-04-26 11:34:36 +0200

    Random moves player

--- source/src/main/java/agh/sao/reversi/algorithm/player/RandomMovesPlayer.java
+++ source/src/main/java/agh/sao/reversi/algorithm/player/RandomMovesPlayer.java
@@ -5,19 +5,27 @@ import agh.sao.reversi.engine.game.Move;
 import agh.sao.reversi.engine.game.PieceColor;
 import agh.sao.reversi.engine.player.IPlayer;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Imiolak on 15-Mar-17.
  */
 public class RandomMovesPlayer implements IPlayer {
+    private final PieceColor playerColor;
+
+    public RandomMovesPlayer(PieceColor playerColor) {
+        this.playerColor = playerColor;
+    }
+
     @Override
     public PieceColor getPlayerColor() {
-        return null;
+        return this.playerColor;
     }
 
     @Override
     public Move chooseMoveToPerform(GameState board, List<Move> availableMoves) {
-        return null;
+        return availableMoves.get(new Random().nextInt(availableMoves.size()));
     }
 }
